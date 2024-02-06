cb1e4016db73de880031f633855488e1f648a7ca
Qasir Mehmood <qasirdev@gmail.com>
2024-02-06 11:21:27 +0000

    resolved turbo build error

--- apps/story-next-ui-doc/src/stories/Button.stories.ts
+++ apps/story-next-ui-doc/src/stories/Button.stories.ts
@@ -15,9 +15,7 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  argTypes: {},
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -28,6 +26,7 @@ export const Primary: Story = {
   args: {
     className: "text-1xl bg-blue-400 font-bold p-3 rounded-md",
     children: 'Click me',
+    appName: 'storybook'
   },
 };
 
@@ -35,6 +34,7 @@ export const Secondary: Story = {
   args: {
     className: "text-1xl bg-green-400 font-bold p-3 rounded-md",
     children: 'Click me',
+    appName: 'storybook'
   },
 };
 
@@ -42,6 +42,7 @@ export const Large: Story = {
   args: {
     className: "text-2xl bg-green-400 font-bold p-3 rounded-md",
     children: 'Click me',
+    appName: 'storybook'
   },
 };
 
@@ -49,5 +50,6 @@ export const Small: Story = {
   args: {
     className: "text-2xm bg-green-400 font-bold p-3 rounded-md",
     children: 'Click me',
+    appName: 'storybook'
   },
 };
--- apps/web/tailwind.config copy.ts
+++ apps/web/tailwind.config copy.ts
@@ -5,7 +5,7 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-// import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
   content: [
